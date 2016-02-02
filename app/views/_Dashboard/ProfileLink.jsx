60413e070dd63ac5edac4f0be9235a228ee07013
Jeffrey <jrhicks@cteh.com>
2016-02-01 22:44:04 -0600

    FlagObject

--- app/views/_Dashboard/ProfileLink.jsx
+++ app/views/_Dashboard/ProfileLink.jsx
@@ -4,15 +4,18 @@ import styles from './ProfileLink.scss';
 class ProfileLink extends React.Component {
 
   render() {
-    const randomUrl = `http://lorempixel.com/20/20/people?x=${Math.random()}`;
+    const img = <i className="material-icons" style={{ fontSize: '1em' }}>face</i>;
+
+    // const randomUrl = `http://lorempixel.com/20/20/people?x=${Math.random()}`;
+    // const img = <img src={randomUrl} />;
 
     return (
       <div className={styles.flag}>
         <div className={styles.image}>
-          <img src={randomUrl} alt="" />
+          {img}
         </div>
         <div className={styles.body}>
-          <p>Foo Bar</p>
+          Foo Bar
         </div>
       </div>
     );
